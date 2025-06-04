b7cd5445c47c731d20f195058bcdc0f70033cde5
Cristhian David Recalde <crecalde@neaternotes.com>
2025-06-04 18:57:51 -0500

    Refactoriza la pantalla de contador: se cambia a un widget Stateful, se añade un AppBar y se implementan botones para incrementar, decrementar y reiniciar el contador.

--- app_intro/lib/main.dart
+++ app_intro/lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.grey),
       home: CounterScreen(),
     );
   }
--- app_intro/lib/presetation/screens/counters/counter_screen.dart
+++ app_intro/lib/presetation/screens/counters/counter_screen.dart
@@ -1,29 +1,72 @@
 import 'package:flutter/material.dart';
 
-class CounterScreen extends StatelessWidget {
-  const CounterScreen({super.key});
+class CounterScreen extends StatefulWidget {
+  CounterScreen({super.key});
 
+  int click = 0;
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Counter"),
+        centerTitle: true,
+        // backgroundColor: Colors.grey,
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              "50",
+              "${widget.click}",
               style: TextStyle(fontSize: 100, fontWeight: FontWeight.w100),
             ),
             Text(
-              "Clicks",
+              "Click${widget.click == 1 ? "" : "s"}",
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.w100),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.plus_one, size: 50),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          SizedBox(width: 10),
+          FloatingActionButton(
+            backgroundColor: Colors.grey[800],
+            onPressed: () {
+              setState(() {
+                widget.click--;
+              });
+            },
+            child: Icon(Icons.remove, color: Colors.white),
+          ),
+          SizedBox(width: 10),
+          FloatingActionButton(
+            backgroundColor: Colors.grey[800],
+            onPressed: () {
+              setState(() {
+                widget.click++;
+              });
+            },
+            child: Icon(Icons.add, color: Colors.white),
+          ),
+          SizedBox(width: 10),
+          FloatingActionButton(
+            backgroundColor: Colors.grey[800],
+            onPressed: () {
+              setState(() {
+                widget.click = 0;
+              });
+            },
+            child: Icon(Icons.refresh, color: Colors.white),
+          ),
+        ],
       ),
     );
   }
