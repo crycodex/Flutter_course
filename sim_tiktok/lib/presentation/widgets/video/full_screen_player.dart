b5431cee4b6042a18fe52707d96e6f17192f4850
Cristhian David Recalde <crecalde@neaternotes.com>
2025-07-19 10:50:33 -0500

    Transforma FullScreenPlayer de StatelessWidget a StatefulWidget e implementa la lógica del reproductor de video utilizando VideoPlayerController. Se añade un FutureBuilder para la inicialización del video y un indicador de carga mientras se reproduce el video.

--- sim_tiktok/lib/presentation/widgets/video/full_screen_player.dart
+++ sim_tiktok/lib/presentation/widgets/video/full_screen_player.dart
@@ -1,14 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
-class FullScreenPlayer extends StatelessWidget {
-
+class FullScreenPlayer extends StatefulWidget {
   final String videoUrl;
   final String caption;
 
-  const FullScreenPlayer({super.key, required this.videoUrl, required this.caption});
+  const FullScreenPlayer({
+    super.key,
+    required this.videoUrl,
+    required this.caption,
+  });
+
+  @override
+  State<FullScreenPlayer> createState() => _FullScreenPlayerState();
+}
+
+class _FullScreenPlayerState extends State<FullScreenPlayer> {
+  late VideoPlayerController videoPlayerController;
+
+  @override
+  void initState() {
+    super.initState();
+    videoPlayerController = VideoPlayerController.asset(widget.videoUrl)
+      ..setVolume(0)
+      ..setLooping(true)
+      ..play();
+  }
+
+  @override
+  void dispose() {
+    videoPlayerController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return FutureBuilder(
+      future: videoPlayerController.initialize(),
+      builder: (context, snapshot) {
+        return const Center(child: CircularProgressIndicator());
+      },
+    );
   }
 }
