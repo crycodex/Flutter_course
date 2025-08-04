4f5e3e82343cadab0cc528e0dbe9d7e427d2e9f1
Cristhian David Recalde <crecalde@neaternotes.com>
2025-08-04 18:29:30 -0500

    Agrega nuevos tipos de tarjetas en cards.screen.dart, incluyendo imágenes y actualiza la visualización para incluir CardType2 y CardType3. Se mejora la estructura de los datos de las tarjetas con nuevos campos.

--- widgets_app/lib/presentation/screens/cards/cards.screen.dart
+++ widgets_app/lib/presentation/screens/cards/cards.screen.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
 
 const cards = <Map<String, dynamic>>{
-  {"elevation": 0.0, "label": "Elevation 0"},
-  {"elevation": 1.0, "label": "Elevation 1"},
-  {"elevation": 2.0, "label": "Elevation 2"},
-  {"elevation": 3.0, "label": "Elevation 3"},
-  {"elevation": 4.0, "label": "Elevation 4"},
-  {"elevation": 5.0, "label": "Elevation 5"},
+  {
+    "elevation": 0.0,
+    "label": "Elevation 0",
+    "image":
+        "https://plus.unsplash.com/premium_photo-1669700572184-edbb6d28b452?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    "elevation": 1.0,
+    "label": "Elevation 1",
+    "image": "https://picsum.photos/250?image=2",
+  },
+  {
+    "elevation": 2.0,
+    "label": "Elevation 2",
+    "image": "https://picsum.photos/250?image=3",
+  },
+  {
+    "elevation": 3.0,
+    "label": "Elevation 3",
+    "image": "https://picsum.photos/250?image=4",
+  },
+  {
+    "elevation": 4.0,
+    "label": "Elevation 4",
+    "image": "https://picsum.photos/250?image=5",
+  },
+  {
+    "elevation": 5.0,
+    "label": "Elevation 5",
+    "image": "https://picsum.photos/250?image=6",
+  },
 };
 
 class CardsScreen extends StatelessWidget {
@@ -29,14 +54,25 @@ class _CardsView extends StatelessWidget {
     return SingleChildScrollView(
       child: Column(
         children: [
+          Text('Card Type 1'),
           ...cards.map(
             (card) =>
                 _CardType1(label: card['label'], elevation: card['elevation']),
           ),
           const SizedBox(height: 30),
+          Text('Card Type 2'),
           ...cards.map(
             (card) =>
-                _CardType1(label: card['label'], elevation: card['elevation']),
+                _CardType2(label: card['label'], elevation: card['elevation']),
+          ),
+          const SizedBox(height: 30),
+          Text('Card Type 3'),
+          ...cards.map(
+            (card) => _CardType3(
+              label: card['label'],
+              elevation: card['elevation'],
+              image: card['image'],
+            ),
           ),
         ],
       ),
@@ -61,6 +97,90 @@ class _CardType1 extends StatelessWidget {
             Align(
               alignment: Alignment.topRight,
               child: IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
+            ),
+            Align(alignment: Alignment.bottomLeft, child: Text(label)),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CardType2 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardType2({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.primaries[0],
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+        side: BorderSide(color: Colors.grey.withOpacity(0.3), width: 4),
+      ),
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
+            ),
+            Align(alignment: Alignment.bottomLeft, child: Text(label)),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CardType3 extends StatelessWidget {
+  final String label;
+  final double elevation;
+  final String image;
+
+  const _CardType3({
+    required this.label,
+    required this.elevation,
+    required this.image,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+        side: BorderSide(color: Colors.grey.withOpacity(0.3), width: 4),
+      ),
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.more_vert),
+                ),
+              ),
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.network(
+                image,
+                fit: BoxFit.cover,
+                height: 200,
+                width: 200,
+              ),
             ),
             Align(alignment: Alignment.bottomLeft, child: Text(label)),
           ],
